9508834383c995395f10076f6e8a1eef06af9f90
YAMASAKI Masahide <masahide.y@gmail.com>
2016-02-08 14:00:32 +0900

    add tcpproxy dir

--- main.go
+++ main.go
@@ -1,151 +1,54 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
-	"io"
-	"log"
-	"net"
 	"time"
 
+	"github.com/masahide/iriguchikun/tcpproxy"
 	"golang.org/x/net/context"
 )
 
 var (
-	listenNetwork        = "tcp"
-	listenAddr           = ":5444"
-	dialNetwork          = "tcp"
-	dialAddr             = "192.168.99.100:3306"
-	dialTimeout          = 5 * time.Second
-	pipeDeadLine         = 120 * time.Second
-	retryTime            = 1 * time.Second
-	maxServerConnections = 2
-	maxClinetConnections = 10
 
 	// Version is version number
 	Version = "dev"
 	// Date is build date
 	Date = ""
+	t    = tcpproxy.TCPProxy{
+		ListenNetwork:        "tcp",
+		ListenAddr:           ":5444",
+		DialNetwork:          "tcp",
+		DialAddr:             "192.168.99.100:3306",
+		DialTimeout:          5 * time.Second,
+		PipeDeadLine:         120 * time.Second,
+		RetryTime:            1 * time.Second,
+		MaxServerConnections: 2,
+		MaxClinetConnections: 10,
+	}
+	showVer bool
 )
 
-func main() {
-	var v bool
-	flag.StringVar(&listenNetwork, "listenNetwork", listenNetwork, "Listen network")
-	flag.StringVar(&listenAddr, "listenAddr", listenAddr, "Listen address")
-	flag.StringVar(&dialNetwork, "dialNetwork", dialNetwork, "Dial network")
-	flag.StringVar(&dialAddr, "dialAddr", dialAddr, "Dial address")
-	flag.DurationVar(&dialTimeout, "dialTimeout", dialTimeout, "Dial timeout")
-	flag.DurationVar(&retryTime, "retryTime", retryTime, "Retry wait time")
-	flag.DurationVar(&pipeDeadLine, "pipeDeadLine", pipeDeadLine, "Pipe dead line wait time")
-	flag.IntVar(&maxServerConnections, "maxServer", maxServerConnections, "Max server connections")
-	flag.IntVar(&maxClinetConnections, "maxClinet", maxClinetConnections, "Max client connections")
-	flag.BoolVar(&v, "version", v, "Show version")
+func init() {
+	flag.StringVar(&t.ListenNetwork, "listenNetwork", t.ListenNetwork, "Listen network")
+	flag.StringVar(&t.ListenAddr, "listenAddr", t.ListenAddr, "Listen address")
+	flag.StringVar(&t.DialNetwork, "dialNetwork", t.DialNetwork, "Dial network")
+	flag.StringVar(&t.DialAddr, "dialAddr", t.DialAddr, "Dial address")
+	flag.DurationVar(&t.DialTimeout, "dialTimeout", t.DialTimeout, "Dial timeout")
+	flag.DurationVar(&t.RetryTime, "retryTime", t.RetryTime, "Retry wait time")
+	flag.DurationVar(&t.PipeDeadLine, "pipeDeadLine", t.PipeDeadLine, "Pipe dead line wait time")
+	flag.IntVar(&t.MaxServerConnections, "maxServer", t.MaxServerConnections, "Max server connections")
+	flag.IntVar(&t.MaxClinetConnections, "maxClinet", t.MaxClinetConnections, "Max client connections")
+	flag.BoolVar(&showVer, "version", showVer, "Show version")
 	flag.Parse()
-	if v {
+}
+
+func main() {
+	if showVer {
 		fmt.Printf("version: %s %s\n", Version, Date)
 		return
 	}
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-	mainLoop(ctx)
-}
-
-func mainLoop(ctx context.Context) {
-	clientCh := make(chan *net.TCPConn, maxClinetConnections)
-	for i := 0; i < maxServerConnections; i++ {
-		go dial(ctx, clientCh)
-	}
-	addr, err := net.ResolveTCPAddr(listenNetwork, listenAddr)
-	errFatal(err)
-	l, err := net.ListenTCP(listenNetwork, addr)
-	errFatal(err)
-	defer closeConn(l)
-	for {
-		conn, err := l.AcceptTCP()
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		errPrint(conn.SetKeepAlive(true))
-		errPrint(conn.SetKeepAlivePeriod(10 * time.Second))
-		clientCh <- conn
-	}
-}
-
-func dial(ctx context.Context, clientCh chan *net.TCPConn) {
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case client := <-clientCh:
-			dialToPipe(ctx, client)
-		}
-	}
-}
-
-func dialToPipe(ctx context.Context, client *net.TCPConn) {
-	defer closeConn(client)
-	svConn, err := openSvConn()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer closeConn(svConn)
-	deadline := time.Now().Add(pipeDeadLine)
-	if err := svConn.SetDeadline(deadline); err != nil {
-		log.Printf("svConn.SetDeadline err:%s", err)
-	}
-	if err := client.SetDeadline(deadline); err != nil {
-		log.Printf("client.SetDeadline err:%s", err)
-	}
-	errch1 := pipe(client, svConn)
-	errch2 := pipe(svConn, client)
-	select {
-	case err = <-errch1:
-	case err = <-errch2:
-	case <-ctx.Done():
-		return
-	}
-	if err != nil && err != io.EOF {
-		log.Printf("pipe err:%s", err)
-	}
-}
-
-func closeConn(c io.Closer) {
-	if err := c.Close(); err != nil {
-		log.Printf("%T Close err:%s ", c, err)
-	}
-}
-
-func openSvConn() (net.Conn, error) {
-	for i := 0; i < 5; i++ {
-		svConn, err := net.DialTimeout(dialNetwork, dialAddr, dialTimeout)
-		if err != nil {
-			log.Printf("dial err:%s, addr:%s", err, dialAddr)
-			time.Sleep(retryTime * time.Duration(i*i))
-			continue
-		}
-		return svConn, nil
-	}
-	return nil, errors.New("dial The retry was give up")
-}
-
-func pipe(out io.Writer, in io.Reader) chan error {
-	errCh := make(chan error, 1)
-	go func() {
-		_, err := io.Copy(out, in)
-		errCh <- err
-	}()
-	return errCh
-}
-func errPrint(err error) {
-	if err != nil {
-		log.Println(err)
-	}
-}
-func errFatal(err error) {
-	if err != nil {
-		log.Fatal(err)
-	}
+	t.MainLoop(ctx)
 }
---
+++ tcpproxy/tcpproxy.go
@@ -0,0 +1,126 @@
+// Package tcpproxy is tcp to tcp proxy
+package tcpproxy
+
+import (
+	"errors"
+	"io"
+	"log"
+	"net"
+	"time"
+
+	"golang.org/x/net/context"
+)
+
+// TCPProxy is main struct
+type TCPProxy struct {
+	ListenNetwork        string
+	ListenAddr           string
+	DialNetwork          string
+	DialAddr             string
+	DialTimeout          time.Duration
+	PipeDeadLine         time.Duration
+	RetryTime            time.Duration
+	MaxServerConnections int
+	MaxClinetConnections int
+}
+
+// MainLoop ctxでキャンセルされるまでloop
+func (t *TCPProxy) MainLoop(ctx context.Context) {
+	clientCh := make(chan *net.TCPConn, t.MaxClinetConnections)
+	for i := 0; i < t.MaxServerConnections; i++ {
+		go t.dialWorker(ctx, clientCh)
+	}
+	addr, err := net.ResolveTCPAddr(t.ListenNetwork, t.ListenAddr)
+	checkFatal(err)
+	l, err := net.ListenTCP(t.ListenNetwork, addr)
+	checkFatal(err)
+	defer closeConn(l)
+	go t.acceptWorker(ctx, l, clientCh)
+	<-ctx.Done()
+}
+
+func (t *TCPProxy) dialWorker(ctx context.Context, clientCh chan *net.TCPConn) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case client := <-clientCh:
+			t.dialToPipe(ctx, client)
+		}
+	}
+}
+
+func (t *TCPProxy) acceptWorker(ctx context.Context, l *net.TCPListener, clientCh chan *net.TCPConn) {
+	for {
+		conn, err := l.AcceptTCP()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		printErr(conn.SetKeepAlive(true))
+		printErr(conn.SetKeepAlivePeriod(10 * time.Second))
+		clientCh <- conn
+	}
+}
+
+func (t *TCPProxy) dialToPipe(ctx context.Context, client *net.TCPConn) {
+	defer closeConn(client)
+	svConn, err := t.openSvConn()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer closeConn(svConn)
+	deadline := time.Now().Add(t.PipeDeadLine)
+	printErr(svConn.SetDeadline(deadline))
+	printErr(client.SetDeadline(deadline))
+	errch1 := pipe(client, svConn)
+	errch2 := pipe(svConn, client)
+	select {
+	case err = <-errch1:
+	case err = <-errch2:
+	case <-ctx.Done():
+		return
+	}
+	if err != nil && err != io.EOF {
+		log.Printf("pipe err:%s", err)
+	}
+}
+
+func (t *TCPProxy) openSvConn() (net.Conn, error) {
+	for i := 0; i < 5; i++ {
+		svConn, err := net.DialTimeout(t.DialNetwork, t.DialAddr, t.DialTimeout)
+		if err != nil {
+			log.Printf("dial err:%s, addr:%s", err, t.DialAddr)
+			time.Sleep(t.RetryTime * time.Duration(i*i))
+			continue
+		}
+		return svConn, nil
+	}
+	return nil, errors.New("dial The retry was give up")
+}
+
+func pipe(out io.Writer, in io.Reader) chan error {
+	errCh := make(chan error, 1)
+	go func() {
+		_, err := io.Copy(out, in)
+		errCh <- err
+	}()
+	return errCh
+}
+
+func closeConn(c io.Closer) {
+	if err := c.Close(); err != nil {
+		log.Printf("%T Close err:%s ", c, err)
+	}
+}
+func printErr(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
+func checkFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
