ed498fed6757ab129131cd212a3a26eda2979e03
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-29 20:19:09 +0900

    Brew formula update for iriguchikun version v1.2.2

--- Formula/iriguchikun.rb
+++ Formula/iriguchikun.rb
@@ -1,9 +1,9 @@
 class Iriguchikun < Formula
   desc "tcp/udp/unix domain socket proxy"
   homepage "https://github.com/masahide/iriguchikun"
-  url "https://github.com/masahide/iriguchikun/releases/download/v1.2.1/iriguchikun_Darwin_x86_64.tar.gz"
-  version "1.2.1"
-  sha256 "10e37ffa5474b7f529f9239e04d73c9ad62bcf05805e5c7df0c5b6db9c96d47b"
+  url "https://github.com/masahide/iriguchikun/releases/download/v1.2.2/iriguchikun_Darwin_x86_64.tar.gz"
+  version "1.2.2"
+  sha256 "a2392f7874fa5ce7ec18367211de9aa0ef29b3706b6db4c6ea208e17570a9590"
 
   def install
     bin.install "iriguchikun"
